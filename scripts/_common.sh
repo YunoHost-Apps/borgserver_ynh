400b4c5725c7a06b98f39cfcfb43ed0db5f9f76e
ljf <ljf+git@grimaud.me>
2021-04-08 20:20:10 +0200

    [enh] Add monitoring options

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,18 +5,25 @@
 #=================================================
 # App package root directory should be the parent folder
 PKG_DIR=$(cd ../; pwd)
+BORG_VERSION=1.1.16
 
-pkg_dependencies="python3-pip python3-dev libacl1-dev libssl-dev liblz4-dev python-jinja2 python3-setuptools python-virtualenv"
+pkg_dependencies="python3-pip python3-dev libacl1-dev libssl-dev liblz4-dev python3-jinja2 python3-setuptools python3-venv python-virtualenv virtualenv libfuse-dev pkg-config"
 
 # Install borg with pip if borg is not here
 install_borg_with_pip () {
+    if [ -d /opt/borg-env ]; then
+        /opt/borg-env/bin/python /opt/borg-env/bin/pip list | grep "Version: $BORG_VERSION" || ynh_secure_remove /opt/borg-env
+    fi
     if [ ! -d /opt/borg-env ]; then
         python3 -m venv /opt/borg-env
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip install borgbackup==1.1.14
+        /opt/borg-env/bin/python /opt/borg-env/bin/pip install wheel
+        /opt/borg-env/bin/python /opt/borg-env/bin/pip install borgbackup[fuse]==$BORG_VERSION
         echo "#!/bin/bash
     /opt/borg-env/bin/python /opt/borg-env/bin/borg \"\$@\"" > /usr/local/bin/borg
-        chmod a+x /usr/local/bin/borg
+        touch "/opt/borg-env/$(ynh_get_debian_release)"
     fi
+    # We need this to be executable by other borg apps
+    chmod a+x /usr/local/bin/borg
 }
 
 #=================================================
@@ -45,16 +52,4 @@ ynh_save_args () {
         fi
         ynh_app_setting_set $app $setting_var "${!var}"
     done
-}
-
-
-
-ynh_configure () {
-    ynh_backup_if_checksum_is_different $2
-    ynh_render_template "${PKG_DIR}/conf/$1.j2" $2
-    ynh_store_file_checksum $2
-}
-
-ynh_read_json () {
-    python3 -c "import sys, json;print(json.load(open('$1'))['$2'])"
 }
