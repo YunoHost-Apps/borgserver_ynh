21981d966dc92b7b953b5cdc4bc2525cb8c6e36d
ljf (zamentur) <zamentur@users.noreply.github.com>
2024-04-10 23:29:40 +0200

    [fix] Bookworm install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BORG_VERSION=1.2.6
+BORG_VERSION=1.2.8
 
 # Install borg with pip if borg is not here
 install_borg_with_pip () {
@@ -9,9 +9,11 @@ install_borg_with_pip () {
     fi
     if [ ! -d /opt/borg-env ]; then
         python3 -m venv /opt/borg-env
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip install wheel
+        /opt/borg-env/bin/python /opt/borg-env/bin/pip install pip -U
+        /opt/borg-env/bin/python /opt/borg-env/bin/pip install setuptools -U
+        /opt/borg-env/bin/python /opt/borg-env/bin/pip install wheel -U
         ynh_print_info --message="Installing/compiling borg, this may take some time..."
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip install borgbackup[fuse]==$BORG_VERSION
+        /opt/borg-env/bin/python /opt/borg-env/bin/pip install borgbackup[pyfuse3]==$BORG_VERSION
         echo "#!/bin/bash
     /opt/borg-env/bin/python /opt/borg-env/bin/borg \"\$@\"" > /usr/local/bin/borg
         touch "/opt/borg-env/$(ynh_get_debian_release)"
