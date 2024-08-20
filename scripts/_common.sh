68d0ee514139fc6705d1968ada2036321be1263f
Félix Piédallu <felix@piedallu.me>
2024-08-20 13:35:40 +0200

    Fix permissions on venv

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,6 +16,10 @@ install_borg_with_pip() {
 
     BORG_VERSION=$(ynh_app_upstream_version)
     ynh_exec_as "$app" "$venvpy" -m pip install borgbackup[pyfuse3]=="$BORG_VERSION"
+
+    # Make venv accessible for every user
+    chmod a+rX "$install_dir"
+    chmod a+rX -R "$install_dir/venv"
 }
 
 create_ssh_config() {
